1be5908d969fa5c0a40d4d4cee5a56e4fcc02fed
Hung Nguyen <hungnguyen.130420@gmail.com>
2024-03-12 21:57:59 +0700

    init project

---
+++ backend-todoapp/src/main/java/org/backend/todoapp/BackendTodoappApplication.java
@@ -0,0 +1,13 @@
+package org.backend.todoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTodoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendTodoappApplication.class, args);
+	}
+
+}
---
+++ backend-todoapp/src/test/java/org/backend/todoapp/BackendTodoappApplicationTests.java
@@ -0,0 +1,13 @@
+package org.backend.todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTodoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
