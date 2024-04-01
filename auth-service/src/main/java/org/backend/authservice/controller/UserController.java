ad70deaf6015a3db96947237871ab9a46b6e238c
Hung Nguyen <hungnguyen.130420@gmail.com>
2024-04-01 21:53:46 +0700

    update

--- auth-service/src/main/java/org/backend/authservice/controller/UserController.java
+++ auth-service/src/main/java/org/backend/authservice/controller/UserController.java
@@ -3,9 +3,10 @@ package org.backend.authservice.controller;
 import org.backend.authservice.model.User;
 import org.backend.authservice.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -15,5 +16,19 @@ public class UserController {
     @PostMapping("/addUser")
     public User addUser(@RequestBody User user) {
         return this.userRepository.save(user);
+    }
+
+    @GetMapping("/users")
+    public List<User> getUsers() {
+        return this.userRepository.findAll();
+    }
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<User> getUserByID(@PathVariable String id) {
+        User user = this.userRepository.findById(id).orElse(null);
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(user);
     }
 }
