489cc4fb170e4c1697813acfd4e4e606718602ab
Hung Nguyen <hungnguyen.130420@gmail.com>
2024-04-01 11:18:35 +0700

    update

--- auth-service/src/main/java/org/backend/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/org/backend/authservice/AuthServiceApplication.java
@@ -2,8 +2,10 @@ package org.backend.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
